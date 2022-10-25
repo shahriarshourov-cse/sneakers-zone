ea3aa5cf4adc480dcf936f50c28806c73406b245
Shahriar Shourov <shahriarshourov@github.com>
2022-10-25 11:46:26 +0600

    added total

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+  }
   return (
     <div className="cart">
       <h3>Purchase Details</h3>
       <p>Selected Items : {cart.length}</p>
+      <p>Price : ${total} </p>
+      <p>Shipping : </p>
+      <p>TAX :</p>
+      <h4>Grand Total : </h4>
     </div>
   );
 };
