d198c26e1d0305f0f1e1b0eac0337b75e36dd21c
Shahriar Shourov <shahriarshourov@github.com>
2022-10-27 01:07:06 +0600

    Load cart from local storage, find product

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,17 +3,22 @@ import "./Cart.css";
 
 const Cart = ({ cart }) => {
   let total = 0;
+  let shipping = 0;
+
   for (const product of cart) {
     total = total + product.price;
+    shipping = shipping + product.shipping;
   }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = (tax + total + shipping).toFixed(2);
   return (
     <div className="cart">
       <h3>Purchase Details</h3>
       <p>Selected Items : {cart.length}</p>
       <p>Price : ${total} </p>
-      <p>Shipping : </p>
-      <p>TAX :</p>
-      <h4>Grand Total : </h4>
+      <p>Shipping : ${shipping}</p>
+      <p>TAX :{tax}</p>
+      <h4>Grand Total: ${grandTotal}: </h4>
     </div>
   );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,5 +1,6 @@
 // import React from "react";
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/demoDb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -12,10 +13,20 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    for (const id in storedCart) {
+      
+        const addedProduct = products.find(product => product.id === id)
+        console.log(addedProduct);
+      }
+    }
+  }, []);
   const addToCart = (product) => {
     console.log(product);
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
   return (
     <div className="shop-container">
